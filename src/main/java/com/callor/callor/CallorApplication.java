72dfeaf854aab18022bef8a42c3509aaf8b58ebe
windowyoul <windowyoul@gmail.com>
2022-08-12 09:48:13 +0900

    08/12

---
+++ src/main/java/com/callor/callor/CallorApplication.java
@@ -0,0 +1,13 @@
+package com.callor.callor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallorApplication.class, args);
+	}
+
+}
